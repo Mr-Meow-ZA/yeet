517a4393cf8336dbae310a91cb2783b2e7b68f75
Mr-Meow-ZA <108538294+Mr-Meow-ZA@users.noreply.github.com>
2022-07-16 11:49:09 +0200

    removed upper decorators

--- main.ts
+++ main.ts
@@ -1,11 +1,5 @@
-/**
- * this is the yeet functionality blocks
- */
-//% color = #02f588 weight = 200
-//% groups = '["yeet1","yeet2"]'
 namespace yeetus{
 
-//% group ="yeet1"
 //% block = "showMiddlePin"
 export function showMiddlePin(x:number, y:number): void
 {
