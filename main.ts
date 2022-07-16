dc188296119bb38c99a9c5da1ed42becb90352ec
Mr-Meow-ZA <108538294+Mr-Meow-ZA@users.noreply.github.com>
2022-07-16 11:47:41 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 /**
  * this is the yeet functionality blocks
  */
-//% color = #02f588 weight = 20000
+//% color = #02f588 weight = 200
 //% groups = '["yeet1","yeet2"]'
 namespace yeetus{
 
 //% group ="yeet1"
-//% block 
+//% block = "showMiddlePin"
 export function showMiddlePin(x:number, y:number): void
 {
     led.plot(x,y)
