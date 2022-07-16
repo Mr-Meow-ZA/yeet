cd6c2984eafcb702a24e65d9b0d51efde4113fa8
Mr-Meow-ZA <108538294+Mr-Meow-ZA@users.noreply.github.com>
2022-07-16 11:55:52 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,13 +2,15 @@
  * Custom blocks
  */
 //% weight=10 color=#20bd93
+//% groups = '["hello"]'
 namespace evenNumberChecker {
 
     /**
      *  Checks if a number is even
      */
-    
+    //% blockID = CheckIfANumberIsEven
     //% block = "checkeven"
+    //% group = "hello"
     export function checkEven(num: number): boolean {
         let result = false;
         let value = num % 2;
